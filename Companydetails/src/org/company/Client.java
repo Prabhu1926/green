3ff442107611e3c4ed0cca077e5aeead7b2943a4
Prabhu M <Prabhu M@DESKTOP-O6VVFQB>
2019-04-17 15:52:55 +0530

    First Commit
    Uploading All files

---
+++ Companydetails/src/org/company/Client.java
@@ -0,0 +1,8 @@
+package org.company;
+
+public class Client {
+	public void clientname() {
+		System.out.println(" client name was RAJU");
+	}
+
+}
---
+++ Companydetails/src/org/company/Company.java
@@ -0,0 +1,16 @@
+package org.company;
+
+public class Company {
+	public void companyname() {
+		System.out.println("The company name was Robert bosch ");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Company cn=new Company();
+		cn.companyname();
+		Client cl=new Client();
+		cl.clientname();
+	}
+
+}
